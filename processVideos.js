73c29f76afa7d9e4d400995df53769ddcf0e1272
binRick <rapidvps@gmail.com>
2015-06-07 21:08:10 -0400

    new

--- processVideos.js
+++ processVideos.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var _ = require('underscore'),
-c = require('chalk'),
+    c = require('chalk'),
     trim = require('trim'),
     child = require('child_process'),
     stripAnsi = require('strip-ansi'),
@@ -33,14 +33,28 @@ process.stdin.on('readable', function() {
             .version('0.0.1')
             .option('-i, --image [image]', 'Image', 'image')
             .option('-l, --limit [limit]', 'Parallel Limit', 'limit')
+            .option('-m, --mode [limit]', 'Mode', 'mode')
+            .option('-e, --extension [limit]', 'Extension', 'extension')
             .parse(process.argv);
         var tasks = [];
         _.each(mp3s, function(mp3) {
             var m = path.parse(mp3);
+            var X = 1,
+                TITLE = 2,
+                DESC = 3;
             tasks.push(function(cb) {
-                var cmd = 'ls ' + m.name + '.mkv 2>/dev/null || ffmpeg -loop 1 -r ntsc -i ' + program.image + ' -i ' + mp3 + ' -c:a copy -c:v libx264 -preset fast -threads 0 -shortest ' + m.name + '.mkv';
+                switch (program.mode) {
+                    case 'processMp3s':
+                        var cmd = 'ls ' + m.name + '.mkv 2>/dev/null || ffmpeg -loop 1 -r ntsc -i ' + program.image + ' -i ' + mp3 + ' -c:a copy -c:v libx264 -preset fast -threads 0 -shortest ' + m.name + '.mkv';
+                        break;
+                    case 'processVideos':
+                        var cmd = "ls " + mp3 + " && echo ./yt.js " + X + " " + X + " " + mp3 + " '" + TITLE + "' '" + DESC + "' && mv " + mp3 + " " + mp3 + ".uploaded ; sleep 5";
+                        break;
+                }
+                console.log(cmd);
+                process.exit();
                 var start = new Date().getTime();
-console.log(c.green('running command'), c.black.bgWhite(cmd));
+                console.log(c.green('running command'), c.black.bgWhite(cmd));
                 child.exec(cmd, function(e, stde, stdo) {
                     if (e) throw e;
                     var o = {
@@ -49,18 +63,14 @@ console.log(c.green('running command'), c.black.bgWhite(cmd));
                         out: String(stdo).split('\n'),
                         err: String(stde).split('\n'),
                     };
-                    console.log(o);
-//fs.writeFileSync(__dirname+'/'+m.name+'.mp3-to-mkv.json', JSON.stringify(o));
-
-
+                    console.log(o.out, cmd, typeof(stdo), stdo.length);
                     cb(null, o);
-
                 });
             });
         });
         async.mapLimit(tasks, parseInt(program.limit), taskIterator, function(e, Results) {
             if (e) throw e;
-            console.log(Results);
+            console.log(pj.render(Results));
         });
     }
 });
