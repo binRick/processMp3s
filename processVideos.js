da3504f198130ab699407691e29f50ff306876ec
binRick <rapidvps@gmail.com>
2015-06-07 22:18:04 -0400

    inew

--- processVideos.js
+++ processVideos.js
@@ -35,24 +35,63 @@ process.stdin.on('readable', function() {
             .option('-l, --limit [limit]', 'Parallel Limit', 'limit')
             .option('-m, --mode [limit]', 'Mode', 'mode')
             .option('-e, --extension [limit]', 'Extension', 'extension')
+            .option('-d, --debug', 'Debug', 'debug')
+            .option('-t, --titlefunction [titlefunction]', 'Title Function', 'titlefunction')
             .parse(process.argv);
         var tasks = [];
+        if (program.debug) {
+            console.log(program);
+            process.exit();
+        }
         _.each(mp3s, function(mp3) {
             var m = path.parse(mp3);
             var X = 1,
                 TITLE = 2,
                 DESC = 3;
+            if (program.titlefunction) {
+
+                var fn = new Function('file', program.titlefunction);
+                var tt = fn(mp3);
+                console.log(program.titlefunction);
+                console.log(fn, tt);
+                var TITLE = tt;
+            } else {
+                                TITLE = m.name.split('_2')[0];
+            }
+            TITLE = TITLE.replace(/_/g, ' ');
+            var DATE = m.name.split('_2')[1].split('.');
+            var D = {
+                Y: '2' + DATE[0],
+                M: DATE[1],
+                D: DATE[2]
+            };
+            var l = m.name.split('.');
+            var GUEST = l[l.length - 1];
+            GUEST = GUEST.replace(/_/g, ' ');
+
+            var Filer = {
+                Date: D,
+                Guest: GUEST,
+                Title: TITLE,
+                File: mp3,
+
+            };
+            var d = Filer.Date.Y + '-' + Filer.Date.M + '-' + Filer.Date.D;
+            Filer.Title = d + ' ' + Filer.Title;
+            Filer.Desc = Filer.Title + ' With Guest ' + Filer.Guest + ' on ' + d;
+            console.log(Filer);
+            process.exit();
             tasks.push(function(cb) {
                 switch (program.mode) {
                     case 'processMp3s':
                         var cmd = 'ls ' + m.name + '.mkv 2>/dev/null || ffmpeg -loop 1 -r ntsc -i ' + program.image + ' -i ' + mp3 + ' -c:a copy -c:v libx264 -preset fast -threads 0 -shortest ' + m.name + '.mkv';
                         break;
                     case 'processVideos':
-                        var cmd = "ls " + mp3 + " && echo ./yt.js " + X + " " + X + " " + mp3 + " '" + TITLE + "' '" + DESC + "' && mv " + mp3 + " " + mp3 + ".uploaded ; sleep 5";
+                        var cmd = "ls " + mp3 + " && ./yt.js " + process.env['KEY'] + " " + process.env.TOKEN + " " + mp3 + " '" + Filer.Title + "' '" + Filer.Desc + "' && mv " + mp3 + " " + mp3 + ".uploaded ; sleep 5";
                         break;
                 }
-                console.log(cmd);
-                process.exit();
+                //                console.log(cmd);
+                //                process.exit();
                 var start = new Date().getTime();
                 console.log(c.green('running command'), c.black.bgWhite(cmd));
                 child.exec(cmd, function(e, stde, stdo) {
